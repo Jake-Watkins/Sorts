84268d3705882a348e9c61b7b19e17672eacdbf0
jake <watkinj7@wwu.edu>
2019-03-06 12:11:54 -0800

    skeleton code finished

--- src/Sorting.java
+++ src/Sorting.java
@@ -1,133 +1,196 @@
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-public class Sorting
+class Sorting
 {
 	//TODO: Implement selectionSort
-	public Comparable[] selectionSort(Comparable[] arr){
+	static Comparable[] selectionSort(Comparable[] arr)
+	{
 		throw new NotImplementedException();
 	}
+
 	//TODO: Implement bubbleSort
-	public Comparable[] bubbleSort(Comparable[] arr){
+	static Comparable[] bubbleSort(Comparable[] arr)
+	{
 		throw new NotImplementedException();
 	}
+
 	//TODO: Implement recursiveBubbleSort
-	public Comparable[] recursiveBubbleSort(Comparable[] arr){
+	static Comparable[] recursiveBubbleSort(Comparable[] arr)
+	{
 		throw new NotImplementedException();
 	}
+
 	//TODO: Implement insertionSort
-	public Comparable[] insertionSort(Comparable[] arr){
+	static Comparable[] insertionSort(Comparable[] arr)
+	{
 		throw new NotImplementedException();
 	}
+
 	//TODO: Implement recursiveInsertionSort
-	public Comparable[] recursiveInsertionSort(Comparable[] arr){
+	static Comparable[] recursiveInsertionSort(Comparable[] arr)
+	{
 		throw new NotImplementedException();
 	}
+
 	//TODO: Implement mergeSort
-	public Comparable[] mergeSort(Comparable[] arr){
+	static Comparable[] mergeSort(Comparable[] arr)
+	{
 		throw new NotImplementedException();
 	}
+
 	//TODO: Implement iterativeMergeSort
-	public Comparable[] iterativeMergeSort(Comparable[] arr){
+	static Comparable[] iterativeMergeSort(Comparable[] arr)
+	{
 		throw new NotImplementedException();
 	}
+
 	//TODO: Implement quickSort
-	public Comparable[] quickSort(Comparable[] arr){
+	static Comparable[] quickSort(Comparable[] arr)
+	{
 		throw new NotImplementedException();
 	}
+
 	//TODO: Implement iterativeQuickSort
-	public Comparable[] iterativeQuickSort(Comparable[] arr){
+	static Comparable[] iterativeQuickSort(Comparable[] arr)
+	{
 		throw new NotImplementedException();
 	}
+
 	//TODO: Implement heapSort
-	public Comparable[] heapSort(Comparable[] arr){
+	static Comparable[] heapSort(Comparable[] arr)
+	{
 		throw new NotImplementedException();
 	}
+
 	//TODO: Implement countingSort
-	public Comparable[] countingSort(Comparable[] arr){
+	static Comparable[] countingSort(Comparable[] arr)
+	{
 		throw new NotImplementedException();
 	}
+
 	//TODO: Implement radixSort
-	public Comparable[] radixSort(Comparable[] arr){
+	static Comparable[] radixSort(Comparable[] arr)
+	{
 		throw new NotImplementedException();
 	}
+
 	//TODO: Implement bucketSort
-	public Comparable[] bucketSort(Comparable[] arr){
+	static Comparable[] bucketSort(Comparable[] arr)
+	{
 		throw new NotImplementedException();
 	}
+
 	//TODO: Implement shellSort
-	public Comparable[] shellSort(Comparable[] arr){
+	static Comparable[] shellSort(Comparable[] arr)
+	{
 		throw new NotImplementedException();
 	}
+
 	//TODO: Implement timSort
-	public Comparable[] timSort(Comparable[] arr){
+	static Comparable[] timSort(Comparable[] arr)
+	{
 		throw new NotImplementedException();
 	}
+
 	//TODO: Implement combSort
-	public Comparable[] combSort(Comparable[] arr){
+	static Comparable[] combSort(Comparable[] arr)
+	{
 		throw new NotImplementedException();
 	}
+
 	//TODO: Implement pigeonSort
-	public Comparable[] pigeonSort(Comparable[] arr){
+	static Comparable[] pigeonSort(Comparable[] arr)
+	{
 		throw new NotImplementedException();
 	}
+
 	//TODO: Implement cycleSort
-	public Comparable[] cycleSort(Comparable[] arr){
+	static Comparable[] cycleSort(Comparable[] arr)
+	{
 		throw new NotImplementedException();
 	}
+
 	//TODO: Implement cocktailSort
-	public Comparable[] cocktailSort(Comparable[] arr){
+	static Comparable[] cocktailSort(Comparable[] arr)
+	{
 		throw new NotImplementedException();
 	}
+
 	//TODO: Implement strandSort
-	public Comparable[] strandSort(Comparable[] arr){
+	static Comparable[] strandSort(Comparable[] arr)
+	{
 		throw new NotImplementedException();
 	}
+
 	//TODO: Implement bitonicSort
-	public Comparable[] bitonicSort(Comparable[] arr){
+	static Comparable[] bitonicSort(Comparable[] arr)
+	{
 		throw new NotImplementedException();
 	}
+
 	//TODO: Implement pancakeSort
-	public Comparable[] pancakeSort(Comparable[] arr){
+	static Comparable[] pancakeSort(Comparable[] arr)
+	{
 		throw new NotImplementedException();
 	}
+
 	//TODO: Implement binaryInsertionSort
-	public Comparable[] binaryInsertionSort(Comparable[] arr){
+	static Comparable[] binaryInsertionSort(Comparable[] arr)
+	{
 		throw new NotImplementedException();
 	}
+
 	//TODO: Implement bogoSort
-	public Comparable[] bogoSort(Comparable[] arr){
+	static Comparable[] bogoSort(Comparable[] arr)
+	{
 		throw new NotImplementedException();
 	}
+
 	//TODO: Implement gnomeSort
-	public Comparable[] gnomeSort(Comparable[] arr){
+	static Comparable[] gnomeSort(Comparable[] arr)
+	{
 		throw new NotImplementedException();
 	}
+
 	//TODO: Implement sleepSort
-	public Comparable[] sleepSort(Comparable[] arr){
+	static Comparable[] sleepSort(Comparable[] arr)
+	{
 		throw new NotImplementedException();
 	}
+
 	//TODO: Implement stoogeSort
-	public Comparable[] stoogeSort(Comparable[] arr){
+	static Comparable[] stoogeSort(Comparable[] arr)
+	{
 		throw new NotImplementedException();
 	}
+
 	//TODO: Implement treeSort
-	public Comparable[] treeSort(Comparable[] arr){
+	static Comparable[] treeSort(Comparable[] arr)
+	{
 		throw new NotImplementedException();
 	}
+
 	//TODO: Implement cartesianTreeSort
-	public Comparable[] cartesianTreeSort(Comparable[] arr){
+	static Comparable[] cartesianTreeSort(Comparable[] arr)
+	{
 		throw new NotImplementedException();
 	}
+
 	//TODO: Implement brickSort
-	public Comparable[] brickSort(Comparable[] arr){
+	static Comparable[] brickSort(Comparable[] arr)
+	{
 		throw new NotImplementedException();
 	}
+
 	//TODO: Implement threeWayQuickSort
-	public Comparable[] threeWayQuickSort(Comparable[] arr){
+	static Comparable[] threeWayQuickSort(Comparable[] arr)
+	{
 		throw new NotImplementedException();
 	}
+
 	//TODO: Implement threeWayMergeSort
-	public Comparable[] threeWayMergeSort(Comparable[] arr){
+	static Comparable[] threeWayMergeSort(Comparable[] arr)
+	{
 		throw new NotImplementedException();
 	}
 }
--- src/Testing.java
+++ src/Testing.java
@@ -1,26 +1,100 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Random;
 import java.util.function.Function;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class Testing
 {
-	private static Map<String, Function<Comparable, Comparable>> commands;
-	public static void main(String[] args){
-		Integer[] arr = new Integer[1024];
-		Random rand = new Random();
-		for(int i = 0;i<arr.length;i++){
-			arr[i] = rand.nextInt();
-		}
-		
+	private static Map<String, Function<Comparable[], Comparable[]>> commands;
+	private static Integer[] scrambled;
+	private static Integer[] sorted;
+	public static void main(String[] args)
+	{
+		buildArray();
 		buildCommands();
+		testFunctions();
 	}
 
-	//TODO: Implement
+	private static void testFunctions()
+	{
+		for (Map.Entry<String, Function<Comparable[], Comparable[]>> entry : commands.entrySet())
+		{
+			try
+			{
+				Integer[] output = (Integer[]) entry.getValue().apply(scrambled);
+				if(compareOutputs(output, sorted)){
+					System.out.printf("%s returned correctly%n",entry.getKey());
+				}
+				else{
+
+					System.out.printf("%s returned incorrectly%n",entry.getKey());
+				}
+			}
+			catch (NotImplementedException e){
+				System.out.printf("%s not implemented%n",entry.getKey());
+			}
+		}
+	}
+
+	private static boolean compareOutputs(Integer[] output, Integer[] sorted)
+	{
+		for(int i = 0;i<output.length;i++){
+			if(!Objects.equals(output[i], sorted[i])){
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private static void buildArray()
+	{
+		scrambled = new Integer[1024];
+		Random rand = new Random();
+		for (int i = 0; i < scrambled.length; i++)
+		{
+			scrambled[i] = rand.nextInt();
+		}
+		sorted = scrambled.clone();
+		Arrays.sort(sorted);
+	}
+
 	private static void buildCommands()
 	{
 		commands = new HashMap<>();
-		throw new NotImplementedException();
+		commands.put("selectionSort",(Comparable[] input) -> Sorting.selectionSort(input));
+		commands.put("bubbleSort",(Comparable[] input) -> Sorting.bubbleSort(input));
+		commands.put("recursiveBubbleSort",(Comparable[] input) -> Sorting.recursiveBubbleSort(input));
+		commands.put("insertionSort",(Comparable[] input) -> Sorting.insertionSort(input));
+		commands.put("recursiveInsertionSort",(Comparable[] input) -> Sorting.recursiveInsertionSort(input));
+		commands.put("mergeSort",(Comparable[] input) -> Sorting.mergeSort(input));
+		commands.put("iterativeMergeSort",(Comparable[] input) -> Sorting.iterativeMergeSort(input));
+		commands.put("quickSort",(Comparable[] input) -> Sorting.quickSort(input));
+		commands.put("iterativeQuickSort",(Comparable[] input) -> Sorting.iterativeQuickSort(input));
+		commands.put("heapSort",(Comparable[] input) -> Sorting.heapSort(input));
+		commands.put("countingSort",(Comparable[] input) -> Sorting.countingSort(input));
+		commands.put("radixSort",(Comparable[] input) -> Sorting.radixSort(input));
+		commands.put("bucketSort",(Comparable[] input) -> Sorting.bucketSort(input));
+		commands.put("shellSort",(Comparable[] input) -> Sorting.shellSort(input));
+		commands.put("timSort",(Comparable[] input) -> Sorting.timSort(input));
+		commands.put("combSort",(Comparable[] input) -> Sorting.combSort(input));
+		commands.put("pigeonSort",(Comparable[] input) -> Sorting.pigeonSort(input));
+		commands.put("cycleSort",(Comparable[] input) -> Sorting.cycleSort(input));
+		commands.put("cocktailSort",(Comparable[] input) -> Sorting.cocktailSort(input));
+		commands.put("strandSort",(Comparable[] input) -> Sorting.strandSort(input));
+		commands.put("bitonicSort",(Comparable[] input) -> Sorting.bitonicSort(input));
+		commands.put("pancakeSort",(Comparable[] input) -> Sorting.pancakeSort(input));
+		commands.put("binaryInsertionSort",(Comparable[] input) -> Sorting.binaryInsertionSort(input));
+		commands.put("bogoSort",(Comparable[] input) -> Sorting.bogoSort(input));
+		commands.put("gnomeSort",(Comparable[] input) -> Sorting.gnomeSort(input));
+		commands.put("sleepSort",(Comparable[] input) -> Sorting.sleepSort(input));
+		commands.put("stoogeSort",(Comparable[] input) -> Sorting.stoogeSort(input));
+		commands.put("treeSort",(Comparable[] input) -> Sorting.treeSort(input));
+		commands.put("cartesianTreeSort",(Comparable[] input) -> Sorting.cartesianTreeSort(input));
+		commands.put("brickSort",(Comparable[] input) -> Sorting.brickSort(input));
+		commands.put("threeWayQuickSort",(Comparable[] input) -> Sorting.threeWayQuickSort(input));
+		commands.put("threeWayMergeSort",(Comparable[] input) -> Sorting.threeWayMergeSort(input));
 	}
 }
